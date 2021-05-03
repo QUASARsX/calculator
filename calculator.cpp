44076579e67df85bb668496fca5aa144159255e3
Hasibul Islam <78943868+QUASARsX@users.noreply.github.com>
2021-05-03 12:02:59 +0600

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -7,38 +7,34 @@ int main()
     float num1; //will hold the foirst input
     float num2; //will hold the second input
     float sum;  //will hold the sum
-    char op; //will hold the operator 
+    char op;   //will hold the operator
 
-    cout << "Please enter two unbers seprated by operator sign: ";
+    cout << "Please enter two number seprated by operator sign: ";
     cin >> num1;
     op = getchar();
     cin >> num2;
     
-    switch (op)
+    switch (op)  //condiotions for different operator
     {
-    case '+' :
+    case '+' :  //if user inputs '+'(plus) sign
         sum = num1 + num2;
         break;
 
-    case '-' :
+    case '-' :  //if user inouts '-'/(minus) sign
         sum = num1 - num2;
         break;
 
-    case '*' :
+    case '*' :  //if user inputs '*'/(multiplication) sign
         sum = num1 * num2;
         break;
 
-    case '/' :
+    case '/' :  //if user inputs '/'(division) sign
         sum = num1 / num2;
-        break;
-
-    default:
-        cout << "operator error!\n";
-        break;
+        break;  //breaks the condition if expresion matches
     }
     
-    cout << ">> " << num1 << op << num2 << " = ";
-    cout << setprecision(3) << fixed << sum << endl;
+    cout << ">> " << num1 << op << num2 << " = "; 
+    cout << setprecision(3) << fixed << sum << endl; //setting final result (sum) to 3 decimal precision point
 
     return 0;
 }
