7bafbb92b440596a45cf170d4861bf3fcaea5804
Hasibul Islam <78943868+QUASARsX@users.noreply.github.com>
2021-05-11 07:05:53 +0600

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,40 +1,47 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+#include <iostream>      //c++ Preprocessor directive for linking the I/O stream Library (To Get Input and Outputing To Console)
+//#include <iomanip>    //c++ Preprocessor directive for linking I/O Map (To set precision points)
+using namespace std;   //c++ Standard Library for standard spacing (Ignors every blank line e.g after opening sign to the closing sign)
 
 int main()
 {
-    float num1; //will hold the foirst input
-    float num2; //will hold the second input
-    float sum;  //will hold the sum
-    char op;   //will hold the operator
+    long double num1;      //holds the first number
+    long double num2;     //holds the second number
+    long double sum;     //holds the operator
+    char op;    
 
     cout << "Please enter two number seprated by operator sign: ";
-    cin >> num1;
-    op = getchar();
-    cin >> num2;
-    
-    switch (op)  //condiotions for different operator
-    {
-    case '+' :  //if user inputs '+'(plus) sign
-        sum = num1 + num2;
-        break;
+    cin >> num1;          //gets the first number
+    op = getchar();      //gets the operator input
+    cin >> num2;        //gets the second number
 
-    case '-' :  //if user inouts '-'/(minus) sign
+    //condiotions for different operator
+    switch (op)  
+    {
+
+    //if user inputs '+'(plus) sign
+    case '+' :  
+        sum = num1 + num2;
+        //breaks the condition if expresion matches
+        break;  
+
+    //if user inputs '-'/(minus) sign
+    case '-' :
         sum = num1 - num2;
         break;
 
-    case '*' :  //if user inputs '*'/(multiplication) sign
+    //if user inputs '*'/(multiplication) sign
+    case '*' :  
         sum = num1 * num2;
         break;
-
-    case '/' :  //if user inputs '/'(division) sign
+        
+    //if user inputs '/'(division) sign
+    case '/' :  
         sum = num1 / num2;
-        break;  //breaks the condition if expresion matches
+        break;
     }
     
-    cout << ">> " << num1 << op << num2 << " = "; 
-    cout << setprecision(2) << fixed << sum << endl; //setting final result (sum) to 3 decimal precision point
+    cout << ">> " << num1 << op << num2 << " = " << sum << '\n';
+    //cout << setprecision(2) << fixed << sum << endl; //setting final result (sum) to 2 decimal precision point
 
-    return 0;
+    return 0;       //returns 0 (an int) to the OS, That means the proggram runs successfully
 }
